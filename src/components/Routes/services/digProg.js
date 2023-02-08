c5a91441b70b5d8e5b582dac3a0556f341cd2495
Shreetej Ghodekar <shreetejghodekar@gmail.com>
2023-02-09 00:59:42 +0530

    Changes to digi prog - Background attached.

--- src/components/Routes/services/digProg.js
+++ src/components/Routes/services/digProg.js
@@ -7,14 +7,14 @@ const DigProg = () => {
   return (
     <div>
       <Container fluid style={{ color: 'black' }}>
-        <Row style={{ 'height': '60vh', 'alignItems': 'end', backgroundImage:`url(${dg})`,backgroundSize:'cover',opacity:'60%' }}>
+        <Row style={{ 'height': '60vh', 'alignItems': 'end', backgroundImage:`url(${dg})`,backgroundSize:'cover',opacity:'60%',backgroundAttachment:'fixed' }}>
           <Col className='m-4' style={{ 'alignContent': 'center' }}>
             <h1 className='mb-4' style={{ 'textAlign': 'center' }}>DIGITAL PROGRAMMING</h1>
           </Col>
         </Row>
-        <Row style={{ 'height': '80vh', 'alignItems': 'center', backgroundColor: 'lavender', backgroundImage:`url(${vc})`,backgroundSize:'cover'}}>
+        <Row style={{ 'height': '80vh', 'alignItems': 'center', backgroundColor: 'lavender', backgroundImage:`url(${vc})`,backgroundSize:'cover',backgroundAttachment:'fixed'}}>
           <Col xs={5} >
-            <div  className='m-4' style={{ 'alignContent': 'center', justifyContent: 'center',color:'black' }}>
+            <div  className='m-4 text-cyan' style={{ 'alignContent': 'center', justifyContent: 'center',color:'black' }}>
               <h2 style={{ 'textAlign': 'center' }}>VIRTUAL COACHING</h2>
               <p style={{ 'textAlign': 'justify', fontSize: '20px' }}>Our virtual coaches utilize the training philosophy developed over years of training elite athletes at the Under Armour Performance Centers powered by FX Fitness. Each custom training program is built on the pillars Move Better, Move Faster, Move Longer, and Move Stronger. </p>
             </div>
