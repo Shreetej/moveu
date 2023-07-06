bec9fda8df4dcbfc84f953bdc0eec1dffc4803fa
Shreetej <33281390+Shreetej@users.noreply.github.com>
2023-07-06 20:39:42 +0530

    Update helper.js
    TP

--- src/components/services/helper.js
+++ src/components/services/helper.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "https://www.moveu.in";
+export const BASE_URL = "https://moveu.in";
 
 export const myAxios = axios.create({
     baseURL: BASE_URL,
