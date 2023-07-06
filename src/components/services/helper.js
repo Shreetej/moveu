92274910087133bc91bed3dc366cfc50740f49c1
Shreetej <33281390+Shreetej@users.noreply.github.com>
2023-07-06 20:33:15 +0530

    Update helper.js
    Changed base url to prod

--- src/components/services/helper.js
+++ src/components/services/helper.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:8082";
+export const BASE_URL = "https://www.moveu.in";
 
 export const myAxios = axios.create({
     baseURL: BASE_URL,
