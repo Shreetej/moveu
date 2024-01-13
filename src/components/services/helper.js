510bf9c54cde392d44f55afaf1814d5660bdd447
Shreetej Ghodekar <shreetejghodekar@gmail.com>
2024-01-13 14:04:30 +0530

    change in url due to change in backend url

--- src/components/services/helper.js
+++ src/components/services/helper.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "https://moveu.onrender.com";
+export const BASE_URL = "https://moveyou-master.onrender.com";
 
 export const myAxios = axios.create({
     baseURL: BASE_URL,
